045b0b92efc6dbaebae871d4567963486d291c12
Ivan Amoshyi <amoshyiivan@gmail.com>
2022-08-17 00:49:34 +0300

    complete

--- src/js/index.js
+++ src/js/index.js
@@ -7,25 +7,38 @@ import template from '../templates/menu.hbs';
 
 const bodyEl = document.body;
 const checkbox = document.querySelector('#theme-switch-toggle');
+const checkboxToggle = document.querySelector('.theme-switch__toggle');
+checkbox.addEventListener('change', changeThemeOnClick);
+
 
 const Theme = {
   LIGHT: 'light-theme',
   DARK: 'dark-theme',
 };
 
-checkbox.addEventListener('change', changeThemeOnClick);
-
 function changeThemeOnClick() {
   if (bodyEl.classList.contains(Theme.DARK)) {
     bodyEl.classList.remove(Theme.DARK);
     bodyEl.classList.add(Theme.LIGHT);
-    localStorage.setItem('theme', Theme.DARK);
+    localStorage.setItem('theme', Theme.LIGHT);
   } else {
     bodyEl.classList.remove(Theme.LIGHT);
     bodyEl.classList.add(Theme.DARK);
-    localStorage.setItem('theme', Theme.LIGHT);
+    localStorage.setItem('theme', Theme.DARK);
   }
+}
 
+currentTheme();
+
+function currentTheme() {
+  const savedTheme = localStorage.getItem('theme');
+
+  if (savedTheme) {
+    bodyEl.classList.add(savedTheme);
+    if (savedTheme === 'dark-theme') {
+      checkboxToggle.setAttribute('checked', true);
+    }
+  }
 }
 
 // ----create HTML----
